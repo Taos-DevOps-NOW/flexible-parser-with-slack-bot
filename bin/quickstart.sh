9d5e14460d1c0ca3776c73a8b3aae0c62ebb651a
Robert R. Meyer <rmeyer@taos.com>
2019-12-18 16:03:42 -0700

    Docker run scripts for easier use - Readme configuration to provide context

--- bin/quickstart.sh
+++ bin/quickstart.sh
@@ -1,10 +1,8 @@
 mkdir config
 cp config.yml.example config/config.yml
 
-CONFIG_PATH="$(pwd)/config"
 
 docker build . -t 2k:slackbot
-docker rm slackbot
-docker run -d -it --name slackbot \
-  --mount type=bind,source=${CONFIG_PATH},target=/opt/slack/config/,readonly \
-  2k:slackbot
+
+bash bin/run_container.sh
+
---
+++ bin/run_container.sh
@@ -0,0 +1,8 @@
+CONFIG_PATH="$(pwd)/config"
+
+
+docker rm slackbot
+
+docker run -d -it --name slackbot \
+  --mount type=bind,source=${CONFIG_PATH},target=/opt/slack/config/,readonly \
+  2k:slackbot
